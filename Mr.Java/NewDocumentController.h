f30333b88fa6dfeb02de6f576f9b2aa10a0ab66a
Justin <Justin@Justin-Browers-MacBook-Pro.local>
2012-07-16 23:41:49 -0400

    Added more classes + more functionality

---
+++ Mr.Java/NewDocumentController.h
@@ -0,0 +1,13 @@
+//
+//  NewDocumentController.h
+//  Mr.Java
+//
+//  Created by Justin Brower on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NewDocumentController : NSWindowController
+
+@end
---
+++ Mr.Java/NewDocumentController.m
@@ -0,0 +1,34 @@
+//
+//  NewDocumentController.m
+//  Mr.Java
+//
+//  Created by Justin Brower on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NewDocumentController.h"
+
+@interface NewDocumentController ()
+
+@end
+
+@implementation NewDocumentController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
