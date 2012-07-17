22a8a516f053a74b15dfae921038946ef9cbb90e
jbrower95 <jbrower95@gmail.com>
2012-07-17 13:21:43 -0400

    added a more detailed system for adding files.. still not complete though

--- Mr.Java/AddController.m
+++ Mr.Java/AddController.m
@@ -32,6 +32,7 @@
   //  [self.window makeKeyAndOrderFront:self.window];
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     [projectName setStringValue:[f lastPathComponent]];
+    [NSApp activateIgnoringOtherApps:YES];
 }
 
 
@@ -39,31 +40,78 @@
 {
     
     // it's a source file. Copy it to our SRC directory.
-    
-    if ( [f hasPrefix:@"file://localhost"] )
-    {
-        
-        f = [f substringFromIndex:16];
-        [f retain];
-        
-    }
+
     
     // as a precondition, MAIN_DIR/SRC must exist.
     NSError *e = nil;
+    NSSound *copy = [NSSound soundNamed:@"copy"];
     
-    [[NSFileManager defaultManager] copyItemAtPath:f toPath:[[mDir stringByAppendingPathComponent:@"SRC"] stringByAppendingPathComponent:[f lastPathComponent]] error:&e];
+    NSString *path = [[NSURL URLWithString:f] path];
+    
+    [[NSFileManager defaultManager] copyItemAtPath:path toPath:[[mDir stringByAppendingPathComponent:@"SRC"] stringByAppendingPathComponent:[f lastPathComponent]] error:&e];
     
     if ( e )
     {
         printf("Could not copy file to SRC :( %s\n",[[e localizedDescription] UTF8String]);
+        
+        if ( [[e localizedDescription] rangeOfString:@"name already exists"].location != NSNotFound)
+        {
+            
+            // the item already exists
+            
+            NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+            [alert setMessageText:[NSString stringWithFormat:@"The specified file already exists! Would you like to overwrite it? (this cannot be undone)"]];
+            [alert addButtonWithTitle:@"Okay"];
+            [alert addButtonWithTitle:@"Cancel"];
+            
+            [alert beginSheetModalForWindow:self.window
+                              modalDelegate:self
+                             didEndSelector:@selector(alertDidEnd:returnCode:contextInfo:)
+                                contextInfo:nil];
+            
+        }
+        
         return;
     }
-    NSSound *copy = [NSSound soundNamed:@"copy"];
     [copy play];
     
     [self close];
     
 }
+
+
+- (void) alertDidEnd:(NSAlert *)a returnCode:(NSInteger)rc contextInfo:(void *)ci {
+    switch(rc) {
+        case NSAlertFirstButtonReturn:
+        {
+       // we're going to proceed with the copy
+            
+            
+            NSString *path = [[NSURL URLWithString:f] path];
+            NSError *e = nil;
+            NSError *er = nil;
+            [[NSFileManager defaultManager] removeItemAtPath:[[mDir stringByAppendingPathComponent:@"SRC"] stringByAppendingPathComponent:[f lastPathComponent]] error:&er];
+            
+              [[NSFileManager defaultManager] copyItemAtPath:path toPath:[[mDir stringByAppendingPathComponent:@"SRC"] stringByAppendingPathComponent:[f lastPathComponent]] error:&e];
+            
+            if ( e == nil && er == nil )
+            {
+                // good to go.
+                [[NSSound soundNamed:@"copy"] play];
+                [self close];
+            }
+            else {
+                // we failed.
+                [[NSSound soundNamed:@"failure"] play];
+                [self close];
+            }
+            
+        }
+    }
+}
+
+
+
 - (IBAction)library:(id)sender
 {
     NSError *e = nil;
